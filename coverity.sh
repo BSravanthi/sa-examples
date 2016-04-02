e3ac04d1ad744055f191a96616440c2e7f586916
Christoph Gerum <christoph@gerum.de>
2016-04-02 06:45:33 +0200

    Fix buffer overflow

--- coverity.sh
+++ coverity.sh
@@ -2,3 +2,12 @@
 
 make clean
 ./cov-analysis-linux64-7.7.0.4/bin/cov-build --dir cov-int make
+
+tar cvzf cov-int.tgz cov-int
+
+curl --form token=bTUr4chdIcwf0aBIZTRn1g \
+  --form email=christoph@gerum.de \
+  --form file=@cov-int.tgz \
+  --form version="0.1" \
+  --form description="master" \
+  https://scan.coverity.com/builds?project=cgerum%2Fsa-examples
